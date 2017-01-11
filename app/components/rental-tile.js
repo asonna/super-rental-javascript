d7587246526be7b3b081a62fedb5bb71ad937ad3
Annie Sonna and Bryant Wang <annie+bryant@>
2017-01-10 17:36:29 -0800

    Refactored code into a rental route to access details for specific rentals.

--- app/components/rental-tile.js
+++ app/components/rental-tile.js
@@ -2,7 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   isImageShowing: false,
-  updateRentalForm: false,
   actions: {
     // imageShow name must match the action reference in the hbs template file
     imageShow: function() {
@@ -10,14 +9,6 @@ export default Ember.Component.extend({
     },
     imageHide: function() {
       this.set('isImageShowing', false);
-    },
-    update(rental, params) {
-      this.sendAction('update', rental, params);
-    },
-    delete(rental) {
-      if (confirm('Are you sure you want to delete this rental?')) {
-        this.sendAction('destroyRental', rental);
-      }
     }
   }
 });
